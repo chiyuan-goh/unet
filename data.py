8fb25ce4140091bb4d11fe2c0274ddd8f66a535d
cy <chiyuan.goh@desay-svautomotive.com>
2018-12-17 18:59:28 +0800

    tmp

--- data.py
+++ data.py
@@ -37,10 +37,14 @@ def adjustData(img,mask,flag_multi_class,num_class):
         new_mask = np.reshape(new_mask,(new_mask.shape[0],new_mask.shape[1]*new_mask.shape[2],new_mask.shape[3])) if flag_multi_class else np.reshape(new_mask,(new_mask.shape[0]*new_mask.shape[1],new_mask.shape[2]))
         mask = new_mask
     elif(np.max(img) > 1):
+        # import pdb
+        # pdb.set_trace()
         img = img / 255
-        mask = mask /255
+        # mask = mask /255
         mask[mask > 0.5] = 1
         mask[mask <= 0.5] = 0
+        # import pdb
+        # pdb.set_trace()
     return (img,mask)
 
 
@@ -85,9 +89,11 @@ def trainGenerator(batch_size,train_path,image_folder,mask_folder,aug_dict,image
 def testGenerator(test_path,num_image = 30,target_size = (256,256),flag_multi_class = False,as_gray = True):
     for i in range(num_image):
         img = io.imread(os.path.join(test_path,"%d.png"%i),as_gray = as_gray)
-        img = img / 255
+        # import pdb
+        # pdb.set_trace()
+        img = img / 255.
         img = trans.resize(img,target_size)
-        img = np.reshape(img,img.shape+(1,)) if (not flag_multi_class) else img
+        #img = np.reshape(img,img.shape+(1,)) if (not flag_multi_class) else img
         img = np.reshape(img,(1,)+img.shape)
         yield img
 
@@ -119,6 +125,9 @@ def labelVisualize(num_class,color_dict,img):
 
 
 def saveResult(save_path,npyfile,flag_multi_class = False,num_class = 2):
+
     for i,item in enumerate(npyfile):
         img = labelVisualize(num_class,COLOR_DICT,item) if flag_multi_class else item[:,:,0]
+        # import pdb
+        # pdb.set_trace()
         io.imsave(os.path.join(save_path,"%d_predict.png"%i),img)
--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 from model import *
 from data import *
+from time import time
+import sys
+from keras.callbacks import TensorBoard
+from utils import MaskImageTensorBoard
 
 #os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
+if __name__ == '__main__':
+	model_name = 'unet_apollo.hdf5'
 
-data_gen_args = dict(rotation_range=0.2,
-                    width_shift_range=0.05,
-                    height_shift_range=0.05,
-                    shear_range=0.05,
-                    zoom_range=0.05,
-                    horizontal_flip=True,
-                    fill_mode='nearest')
-myGene = trainGenerator(2,'data/membrane/train','image','label',data_gen_args,save_to_dir = None)
+	if sys.argv[1] ==  'train':
+		data_gen_args = dict(rotation_range=0.2,
+		                    width_shift_range=0.05,
+		                    height_shift_range=0.05,
+		                    shear_range=0.05,
+		                    zoom_range=0.05,
+		                    horizontal_flip=True,
+		                    fill_mode='nearest')
+		
+		myGene = trainGenerator(4,'data/apollo/train','image','label',data_gen_args,save_to_dir = 'data/apollo/tmp', image_color_mode='rgb')
+		myVal = trainGenerator(4,'data/apollo/train','image','label',data_gen_args,save_to_dir = 'data/apollo/tmp', image_color_mode='rgb')
+		
+		model = unet(input_size = (256, 256, 3))
 
-model = unet()
-model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
-model.fit_generator(myGene,steps_per_epoch=300,epochs=1,callbacks=[model_checkpoint])
+		model_checkpoint = ModelCheckpoint(model_name, monitor='loss',verbose=1, save_best_only=True)
+		tboard = MaskImageTensorBoard(image_every_x_epochs=10, output_name='conv2d_24', log_dir="logs/{}".format(time()), histogram_freq=1)
+		#tboard = TensorBoard(log_dir="logs/{}".format(time()), update_freq = 'epoch')
 
-testGene = testGenerator("data/membrane/test")
-results = model.predict_generator(testGene,30,verbose=1)
-saveResult("data/membrane/test",results)
+		#model.fit_generator(myGene,steps_per_epoch=150,epochs=10,callbacks=[model_checkpoint, tboard], validation_data=myVal, validation_steps=2)
+		model.fit_generator(myGene,steps_per_epoch=10,epochs=10,callbacks=[model_checkpoint, tboard], validation_data=myVal, validation_steps=2)
+	else:
+		model = unet(pretrained_weights=model_name, input_size = (256, 256, 3))
+
+
+	testGene = testGenerator("data/apollo/test", num_image=19, as_gray=False)
+	results = model.predict_generator(testGene,19,verbose=1)
+	saveResult("data/apollo/test",results)
